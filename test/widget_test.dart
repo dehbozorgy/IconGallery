6ddadb57404957a3864db1883bdb4ceaec63a689
Navid <dehbozorgy@gmail.com>
2024-02-22 20:52:19 +0330

    format widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -15,12 +15,10 @@ void main() {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const MyApp());
 
-
     // Verify that our counter starts at 0.
     // expect(find.text('0'), findsOneWidget);
     // expect(find.text('1'), findsNothing);
     expect(find.byType(Scaffold), findsOneWidget);
-
 
     // Tap the '+' icon and trigger a frame.
     // await tester.tap(find.byIcon(Icons.add));
@@ -29,7 +27,5 @@ void main() {
     // Verify that our counter has incremented.
     // expect(find.text('0'), findsNothing);
     // expect(find.text('1'), findsOneWidget);
-
-
   });
 }
